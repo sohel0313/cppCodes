d1f74d586aaf3b4766752ab05eedade7a2c7921b
Ayush Raut <rautayush54@gmail.com>
2022-10-18 10:57:57 +0530

    Changed arrays

--- Array.cpp
+++ Array.cpp
@@ -1,7 +1,15 @@
 #include<iostream>
 using namespace std;
+
+
+void printline(int nums[4]) {
+    for (int i = 0; i < 4; i++) {
+        cout << nums[i] << endl;
+    }
+}
+
 int main(){
     int array[4]={1,2,3,4};
-    cout<<array[3];
+    printline(array);
     return 0;
 }
